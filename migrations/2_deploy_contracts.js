3539a77dc6ec0a3e80a8975ed3c25c2a4b3fe66d
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-05-24 13:23:58 +0700

    migrate file

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,24 @@
+var ECRecovery = artifacts.require("./ECRecovery.sol");
+var SCHToken = artifacts.require("./SCHToken.sol");
+var ERC20Standard = artifacts.require("./ERC20Standard.sol");
+var ChannelLibrary = artifacts.require("./ChannelLibrary.sol");
+var EndpointRegistryContract = artifacts.require("./EndpointRegistryContract.sol");
+var ChannelManagerContract = artifacts.require("./ChannelManagerContract.sol");
+var ChannelApiStub = artifacts.require("./ChannelApiStub.sol");
+
+module.exports = function(deployer) {
+    deployer.deploy(ECRecovery);
+    deployer.deploy(ChannelLibrary);
+    deployer.deploy(EndpointRegistryContract);
+    deployer.link(ECRecovery, ChannelLibrary);
+    deployer.link(ChannelLibrary, ChannelManagerContract);
+
+    deployer.deploy(ERC20Standard)
+    // deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
+    deployer.deploy(ChannelApiStub).then(function() {
+        deployer.deploy(SCHToken, ["0x02c675be7624FC682A95eB29346722E231B34863"],
+        ["1000000"], 1000000).then(function() {
+            deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
+        })
+    });
+};
