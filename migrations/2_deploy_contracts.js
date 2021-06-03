e9975a0be60a157475008a1077da2f82b8b35485
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-06-03 19:41:13 +0700

    run

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -13,14 +13,14 @@ module.exports = function(deployer) {
     deployer.link(ECRecovery, ChannelLibrary);
     deployer.link(ChannelLibrary, ChannelManagerContract);
 
+    deployer.deploy(ERC20Standard);
     deployer.deploy(ChannelApiStub).then(function() {
         deployer.deploy(SCHToken, ["0x02c675be7624FC682A95eB29346722E231B34863"],
-        ["1000000"], 1000000).then(function() {        
-            deployer.deploy(ERC20Standard);
-            deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
+        ["1000000"], 1000000).then(function() {         
+            //deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
         })    
     });
 
     // deployer.deploy(ERC20Standard);
-    //deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
+    deployer.deploy(ChannelManagerContract, SCHToken.address, ChannelApiStub.address);
 }; 
